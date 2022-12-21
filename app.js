6c531d5e200f1650dd50eeb53e50efaf92f712fd
Pradeep Yadav <sochcreativein.com>
2022-12-21 12:47:44 +0530

    new

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express")
 const app = express();
+var cors = require('cors')
 
+app.use(cors())
 const connectDb = require("./db/connect")
 const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
