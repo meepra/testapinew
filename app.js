fe9904ee4be3ac9a3192edf80f3538db4301f1ac
Pradeep Yadav <sochcreativein.com>
2022-12-21 13:09:00 +0530

    new

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-const express = require("express")
-const app = express();
+const express = require("express");
 var cors = require('cors')
+const app = express();
+app.use(cors())
 
 
 const connectDb = require("./db/connect")
@@ -9,12 +10,17 @@ const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
     res.send("hello")
 })
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const products_routes = require("./routes/product")
 // Meddle ware or set router
 
 app.use("/api/products", products_routes)
-app.use(cors())
+
 const start = async () => {
     try {
         await connectDb(process.env.MONGODB_URL);
